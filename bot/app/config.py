1bec363021c7033fed55845d603f9dd047132809
a3 <xyz@mail.com>
2025-12-22 01:32:58 +0300

    init

---
+++ bot/app/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+class Settings(BaseSettings):
+    TG_BOT_TOKEN: str
+
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        extra="ignore",
+    )
+
+
+settings = Settings()
+
+BOT_TOKEN = settings.TG_BOT_TOKEN
+
